9c553503f9e39e500caba612860f1f20de6905a4
kyle41io <vankien41hust@gmail.com>
2022-11-14 00:03:48 +0700

    first commit

---
+++ project1/forms.py
@@ -0,0 +1,32 @@
+from ast import Expression
+from django import forms
+import re
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+class RegistrationForm(forms.Form):
+    username= forms.CharField(label='User Name', max_length=30)
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput())
+    password2 = forms.CharField(label='Rewrite Password', widget=forms.PasswordInput())
+    
+    def clean_password2(self):
+        if 'password1'in self.cleaned_data:
+            password1 = self.cleaned_data['password1']
+            password2 = self.cleaned_data['password1']
+            if password1==password2 and password1:
+                return password2
+        raise forms.ValidationError("Invalid Password")
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError("Invalid Username (Have invalid character)") 
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError("Username have been used") 
+    def save(self):
+        User.objects.create_user(username=self.cleaned_data['username'], email=self.cleaned_data['email'], password=self.cleaned_data['password1'])
+        
+            
